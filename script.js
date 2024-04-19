a77b6d5e26884831f51e0bcf2bef77849fe4af0b
alonso <alonsolm31@gmail.com>
2024-04-18 22:14:56 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let car_1 = document.querySelector("#car_1");
+let row = document.querySelector("#row");
+const card_section = document.querySelector("#card_section");
+
+let startX;
+let scrollTop;
+let isDown = false;
+
+row.addEventListener("mousedown", (e) => mouseDown(e));
+card_section.addEventListener("mouseup", (e) => mouseUp(e));
+card_section.addEventListener("mouseleave", (e) => mouseLeave(e));
+
+window.addEventListener("mousemove", function (e) {
+  if (!isDown) return;
+  let pixel = e.clientX + "px";
+  car_1.style.width = pixel;
+
+  console.log(isDown);
+});
+
+function mouseDown(e) {
+  isDown = true;
+  startX = e.pageX;
+}
+
+function mouseLeave(e) {
+  isDown = false;
+}
+
+function mouseUp(e) {
+  isDown = false;
+}
